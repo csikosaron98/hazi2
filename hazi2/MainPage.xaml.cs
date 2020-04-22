4aa7a6e3e697e75e2dd69d2e1c78ed01ed985aad
Áron Csikós <csikosaron1998@gmail.com>
2020-04-22 10:57:40 +0200

    04.22.

--- hazi2/MainPage.xaml.cs
+++ hazi2/MainPage.xaml.cs
@@ -23,7 +23,7 @@ namespace hazi2
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    /// </summary> 
     public sealed partial class MainPage : Page
     {
         // változók
